137c9f6d1180892474784f214a42172bfbe1ef2c
Roman-sleep <Roman06182000@yandex.ru>
2023-08-15 09:45:33 +0300

    0.33

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,11 +4,11 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'my-default-secret-key')
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
